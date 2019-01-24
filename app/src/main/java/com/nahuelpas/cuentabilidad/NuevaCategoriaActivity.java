900264f561e325b57d0f66917b46608317e56ab5
nahuelPascual <npascual@est.frba.utn.edu.ar>
2019-01-23 21:01:03 -0300

    Validaciones y correcciones en Settings.

--- app/src/main/java/com/nahuelpas/cuentabilidad/NuevaCategoriaActivity.java
+++ app/src/main/java/com/nahuelpas/cuentabilidad/NuevaCategoriaActivity.java
@@ -1,5 +1,6 @@
 package com.nahuelpas.cuentabilidad;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,19 +25,23 @@ public class NuevaCategoriaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nueva_cuenta);
+        setContentView(R.layout.activity_nueva_categoria);
 
         categoriaDao = new CategoriaDao_Impl(Database.getAppDatabase(getApplicationContext()));
         nombreCategoria = findViewById(R.id.et_nombreCategoriaNueva);
-        save = findViewById(R.id.btn_guardarCategoria);
+        save = findViewById(R.id.btn_guardarCategoriaNueva);
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO validar campos vacios
-                categoriaDao.add(new Categoria(categoriaDao.getNextId(),
-                        nombreCategoria.getText().toString()));
-                Toast.makeText(getApplicationContext(), "Categoria guardada exitosamente.", Toast.LENGTH_SHORT).show();
+                String nombre = nombreCategoria.getText().toString();
+                if (nombre!=null && !nombre.isEmpty()) {
+                    categoriaDao.add(new Categoria(categoriaDao.getNextId(), nombre));
+                    Toast.makeText(getApplicationContext(), "Categoria guardada exitosamente.", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                } else {
+                    Toast.makeText(getApplicationContext(), "Revise los datos ingresados.", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
--- app/src/main/java/com/nahuelpas/cuentabilidad/NuevaCuentaActivity.java
+++ app/src/main/java/com/nahuelpas/cuentabilidad/NuevaCuentaActivity.java
@@ -1,5 +1,6 @@
 package com.nahuelpas.cuentabilidad;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,7 @@ import com.nahuelpas.cuentabilidad.model.entities.Cuenta;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.util.StringUtil;
 
 public class NuevaCuentaActivity extends AppCompatActivity {
 
@@ -33,10 +35,16 @@ public class NuevaCuentaActivity extends AppCompatActivity {
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO validar campos vacíos
-                cuentaDao.add(new Cuenta(cuentaDao.getNextId(),
-                        nombreCuenta.getText().toString(), Double.parseDouble(saldoCuenta.getText().toString())));
-                Toast.makeText(getApplicationContext(), "Cuenta guardada exitosamente.", Toast.LENGTH_SHORT).show();
+                String nombre = nombreCuenta.getText().toString();
+                String saldo = saldoCuenta.getText().toString();
+                if (nombre!=null && !nombre.isEmpty() && saldo!=null && !saldo.isEmpty()) {
+                    cuentaDao.add(new Cuenta(cuentaDao.getNextId(),
+                            nombre, Double.parseDouble(saldo)));
+                    Toast.makeText(getApplicationContext(), "Cuenta guardada exitosamente.", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                } else {
+                    Toast.makeText(getApplicationContext(), "Revise los datos ingresados.", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
